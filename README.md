eafcf67fa3708638f4c2326c6f357fd46072f1b1
Nadine <nadine_paez@hotmail.com>
2021-10-09 14:35:56 -0400

    added local storage

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # movies
+This webpage provies the user with a random movie and dinner suggestion.
+
+From a dropdown choice user can choose a genre and click the submit button. Then user will be provided with a random movie and food selection. The webpage will show a movie poster, movie info (title, actors, plot, rating), and a food image.
+
--- js/script.js
+++ js/script.js
@@ -84,9 +84,10 @@ fetch(apiUrl)
     var moviePoster = new Image();
     moviePoster.src = moviePic;
     moviePoster.alt = data['Plot'];
-    moviePoster.setAttribute('style', 'height:350px; width:325px');
+    moviePoster.setAttribute('style', 'height:auto; width:100%');
     movieEl.append(moviePoster);
-
+    // call setMovies to local storage here
+    setMovieToLocalStorage(movieTitle);
     
   });
 
@@ -118,3 +119,46 @@ fetch(foodishApiUrl)
 btn.addEventListener("click", function () {
   getSelectedGenre();
 })
+
+// Save random movie suggestions to localStorage using `setItem()`
+
+function setMovieToLocalStorage(title) {
+  // add movie to local storage
+  var movieArray = JSON.parse(localStorage.getItem('movieTitles')) || [];
+  movieArray.push(title);
+  localStorage.setItem('movieTitles', JSON.stringify(movieArray));
+  getMoviesFromLocalStorage()
+  // CALL get movies from local storage
+}
+
+function getMoviesFromLocalStorage() {
+  // get movies from local storage 
+  var previousResultsHolder = document.getElementById("previous-searches")
+  previousResultsHolder.innerHTML = ""
+  var movieArray = JSON.parse(localStorage.getItem('movieTitles')) || [];
+  if (movieArray.length <= 0) {
+    var h3 = document.createElement("h3")
+    h3.textContent = "no searches yet"
+    previousResultsHolder.appendChild(h3)
+  }
+  else {
+    var ul = document.createElement("ul")
+    for (i = 0; i < movieArray.length; i++) {
+      var li = document.createElement("li")
+      li.textContent = movieArray[i];
+      ul.appendChild(li)
+    }
+    previousResultsHolder.appendChild(ul)
+  }
+  // AND
+  // display them (if none, display no movies searched message)
+}
+
+function clearMoviesFromLocalStorage() {
+  localStorage.clear()
+  getMoviesFromLocalStorage()
+  // clear the local storage
+  // CALL get movies from local storage
+}
+
+document.getElementById("clear-previous-searches").addEventListener("click", clearMoviesFromLocalStorage)
